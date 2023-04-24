872986a88af74a82a1e4897cdba4bcaa880e39df
dave-tree-uk <dave.tree.uk@gmail.com>
2023-04-24 12:55:35 +0100

    Added font colour

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ Created on Mon Apr 24 11:23:50 2023
 """
 
 
-
+'''Ver1.0
 import pygame
 import time
 import sys
@@ -25,3 +25,26 @@ while True:
 
     screen.fill((0, 0, 0))
     current_time = time.strftime('%H:%M:%S')
+'''
+
+import pygame
+import time
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0, 0, 0))
+    current_time = time.strftime('%H:%M:%S')
+    text = font.render(current_time, True, (255, 255, 255))
+    screen.blit(text, (160, 180))
+    pygame.display.update()
+    clock.tick(1)
