31bd939412909706b82bb023475cec6141afeb13
dave-tree-uk <dave.tree.uk@gmail.com>
2023-04-24 13:28:50 +0100

    added window positioning

--- clock.py
+++ clock.py
@@ -3,36 +3,23 @@
 Created on Mon Apr 24 11:23:50 2023
 
 @author: davet
+
+Simple clock in pygame
+
 """
 
 
-'''Ver1.0
 import pygame
 import time
 import sys
+import os
+
+# Set window position
+os.environ['SDL_VIDEO_WINDOW_POS'] = '%d,%d' % (50, 50)
+
 
 pygame.init()
-print('hello')
-screen = pygame.display.set_mode((400, 400))
-clock = pygame.time.Clock()
-font = pygame.font.Font(None, 36)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    screen.fill((0, 0, 0))
-    current_time = time.strftime('%H:%M:%S')
-'''
-
-import pygame
-import time
-import sys
-
-pygame.init()
-screen = pygame.display.set_mode((200, 100))
+screen = pygame.display.set_mode((200, 100), pygame.NOFRAME)
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 
